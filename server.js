4c0e7ec4cf96d0613af01f829702c56fb9eb84c2
Yash Gupta <gyash0440@gmail.com>
2024-08-29 01:32:48 +0530

    get user details

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ app.get('/api/get', (req,res)=>{
     res.send({message:"hello"})
 })
 
+app.get('/api/get_user_details',(req,res) => {
+    res.send({
+        user:{
+            name:"satyam",
+            age:22,
+            contact:12254
+        },env:process.env.NAME
+    })
+});
+
 app.listen(8000,()=>{
     console.log("Port is running")
 })
